f0b7bd1ed779bb6ce0a9208fe37f9d1b3774d6e6
ZombieLieZ <109431569+ZombieLieZ@users.noreply.github.com>
2022-07-16 22:30:54 +0200

    Create mondat.py

---
+++ mondat.py
@@ -0,0 +1,10 @@
+# Ez a kód egy random mondatot fog generálni az általad kitalált szavakból:)
+# Készítette: ZombieLieZ
+név = input("Írj be egy nevet: ")
+ige = input("Írj be egy igét: ")
+idő = input("Írj be egy időpontot (tegnap, ma, holnap): ")
+melléknév = input("Írj be egy melléknevet: ")
+
+print()
+
+print(f"{név} {idő} {melléknév} {ige}!")
